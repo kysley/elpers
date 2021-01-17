ba8fd840b9fd02285bedeea23008018ca3978467
Austin Howlett <austinhowletttech@gmail.com>
2021-01-16 23:23:09 -0500

    feat: added conditional tests for or helper function, fixed linting issues

--- src/or.ts
+++ src/or.ts
@@ -9,32 +9,38 @@ export class OrOptions {
 }
 
 export const or = (
-    comparable: string, 
-    condition1: string, 
-    condition2: string, 
-    opts: OrOptions = new OrOptions()
-  ): boolean => {
+  comparable: string,
+  condition1: string,
+  condition2: string,
+  opts: OrOptions = new OrOptions()
+): boolean => {
   try {
     const normalizedComparable = comparable.toLocaleLowerCase();
     const normalizedCondition1 = condition1.toLocaleLowerCase();
     const normalizedCondition2 = condition2.toLocaleLowerCase();
-  
+
     if (opts.caseSensitive) {
       if (opts.strict) {
         return comparable === condition1 || comparable === condition2;
       }
-  
+
       return comparable == condition1 || comparable == condition2;
     }
-  
+
     if (opts.strict) {
-      return normalizedComparable === normalizedCondition1 || normalizedComparable === normalizedCondition2;
+      return (
+        normalizedComparable === normalizedCondition1 ||
+        normalizedComparable === normalizedCondition2
+      );
     }
-  
-    return normalizedComparable == normalizedCondition1 || normalizedComparable == normalizedCondition2;
+
+    return (
+      normalizedComparable == normalizedCondition1 ||
+      normalizedComparable == normalizedCondition2
+    );
   } catch (error) {
     console.log(error);
     // Fix this, should not return just false
     return false;
   }
-}
+};
